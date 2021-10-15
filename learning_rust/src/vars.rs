e0888c4bfb8d06f9e3cb6dcd18b295dbc9c7f165
jhnegbrt <jhnegbrt@gmail.com>
2021-10-15 12:51:35 -0700

    Multiple assignment

--- learning_rust/src/vars.rs
+++ learning_rust/src/vars.rs
@@ -3,6 +3,15 @@ pub fn run(){
   let mut age = 37;
 
   age = 38;
-  
-  println!("My name is {} and I am {}", name, age)
+
+  println!("My name is {} and I am {}", name, age);
+
+  // Define constant
+  const ID: i32 = 001;
+  println!("ID: {}", ID);
+
+  //assign multiple variables
+  let (my_name, my_age) = ("Brad", 37);
+
+  println!("{} is {}", my_name, my_age);
 }
