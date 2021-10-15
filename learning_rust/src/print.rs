0a26b8fbae9caa2407a86b470bf20cf8f2d1e1b6
jhnegbrt <jhnegbrt@gmail.com>
2021-10-15 12:36:38 -0700

    Adds named properties

--- learning_rust/src/print.rs
+++ learning_rust/src/print.rs
@@ -2,5 +2,6 @@ pub fn run(){
   // Print to console
   println!("Hell from the print file");
   println!("{} is from {}", "Brad", "Mass");
-  println!("{0} is from {1} and {0} likes to {2}", "Brad", "Mass", "Code")
+  println!("{0} is from {1} and {0} likes to {2}", "Brad", "Mass", "Code");
+  println!("{name} like to play {activity}", name = "john", activity = "soccer");
 }
